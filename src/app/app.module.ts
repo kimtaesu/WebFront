13f47febc52e9cef87130f7cf88ec0da5d3e8832
taesu <kimtaesoo188@gmail.com>
2017-03-28 20:15:08 +0900

    material design 셋팅

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
@@ -22,6 +22,7 @@ import {JoinComponent} from "./join/join.component";
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MaterialModule,
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  LoginForm: FormGroup
+
+  ngOnInit(): void {
+    this.LoginForm = new FormGroup({
+      email: new FormControl('', Validators.compose([Validators.required, ValidationService.emailValidator])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      userName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ])),
+    });
+  }
 
 }

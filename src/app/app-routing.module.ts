a580aa9925cba33e5b7f5482cf9d2fd698fb88f1
taesu <kimtaesoo188@gmail.com>
2017-03-28 18:21:35 +0900

    material design 적용

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+      {
+        path: '**',
+        redirectTo: '/login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {MaterialModule} from "@angular/material";
+import "hammerjs";
+import {AppRoutingModule} from "./app-routing.module";
+import {LoginComponent} from "./login/login.component";
 @NgModule({
   declarations: [
+    LoginComponent,
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
